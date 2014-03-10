a6b8c7f18a8199d6bf334ba1aad54b535cdd2192
student <student@debian>
2014-03-10 14:11:28 +0000

    updated database config

--- Hallam5League/grails-app/conf/DataSource.groovy
+++ Hallam5League/grails-app/conf/DataSource.groovy
@@ -15,9 +15,16 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+                    dataSource {
+            dbCreate = "update"
+            driverClassName = "com.mysql.jdbc.Driver"
+            dialect=org.hibernate.dialect.MySQL5Dialect
+            username = "webarch"
+            password = "webarch"
+            url = "jdbc:mysql://localhost/Hallam5League?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8"
+            pooled = true
         }
+}
     }
     test {
         dataSource {
