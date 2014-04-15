342f2b4b4bde12f2168631c52c2b97c99db39b42
student <student@debian>
2014-04-15 17:53:06 +0100

    Update comments for controllers

--- Hallam5League/grails-app/controllers/hallam5league/LeagueCreatorController.groovy
+++ Hallam5League/grails-app/controllers/hallam5league/LeagueCreatorController.groovy
@@ -4,10 +4,10 @@ import league.*
 class LeagueCreatorController {
 
     def index() { 
-	def result = [:]
+	def result = [:]//result mapping. sets result but puts no data in it
       println("Index ${params}");
 	result.l = League.findAll('from league.League as l where l.leagueName like ?',['%'])
-
+	//finds all the leagues that are in the database so they can be displayed in the view
 
 return result
     }
@@ -19,12 +19,13 @@ return result
                              leagueStartDate : params.date('startDate','yyyy-MM-dd'),
                              noOfTeams : params.noOfTeams,
 		             lengthOfLeague : params.lengthOfLeague)
-	if ( b.save() ) {
-          println("Saved OK");
+	//create a new league with the parameters that were passed through the form
+	if ( b.save() ) {//check to see if the save was successful
+          println("Saved OK");//print ok in terminal if it was
         }
-        else {
-          b.errors.each { error ->
-            println("Error: ${error}");
+        else {//condition if not successful
+          b.errors.each { error ->//looping through errors of submit
+            println("Error: ${error}");//print all errors in terminal if not.
           }
         }
 
--- Hallam5League/grails-app/controllers/hallam5league/TeamCreatorController.groovy
+++ Hallam5League/grails-app/controllers/hallam5league/TeamCreatorController.groovy
@@ -4,10 +4,10 @@ import league.*
 class TeamCreatorController {
 
     def index() { 
-	def result = [:]
+	def result = [:]//result mapping. sets result but puts no data in it
       println("Index ${params}");
 	result.l = Team.findAll('from league.Team as l where l.teamName like ?',['%'])
-	
+	//finds all the teams that are in the database so they can be displayed in the view
 
 
 return result
@@ -16,20 +16,21 @@ return result
 
     def create() {
       println(params);
-	if(request.method=="POST"){
+	if(request.method=="POST"){//Checks to see if there has been a request of post
 	def b = new league.Team(teamName : params.teamName, 
                          teamDescription : params.teamDescription,
                          teamMembers : params.teamMembers, 
 			 league : League.get(params.leagueName))
-	if ( b.save() ) {
-          println("Saved OK");
+	//create a new league with the parameters that were passed through the form
+	if ( b.save() ) {//check to see if the save was successful
+          println("Saved OK");//print ok in terminal if it was
         }
-        else {
-          b.errors.each { error ->
-            println("Error: ${error}");
+        else {//condition if not successful
+          b.errors.each { error ->//looping through errors of submit
+            println("Error: ${error}");//print all errors in terminal if not.
           }
         }
-        redirect action: 'index'
+        redirect action: 'index'//redirect to the index page
        }
 
     }
