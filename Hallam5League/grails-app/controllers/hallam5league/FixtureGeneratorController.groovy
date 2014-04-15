81a14d711610f591f70e831135ae7333524a7e6a
student <student@debian>
2014-04-15 17:50:51 +0100

    Commented Fixture generator controller

--- Hallam5League/grails-app/controllers/hallam5league/FixtureGeneratorController.groovy
+++ Hallam5League/grails-app/controllers/hallam5league/FixtureGeneratorController.groovy
@@ -28,13 +28,14 @@ class FixtureGeneratorController {
 	result.r = Team.findAll("from league.Team as r where r.league.id = '${f}' ")
 	//This will get all the teams which are playing in the selected league (by the ID that has been POSTed)
 	result.fixtures = [:]
-	//?
+	//?????
 	def count = 1
 	//Set the count variable at 1 on the first run of the page
 	def fixture_Date = System.currentTimeMillis()
 	//This will set a fixture_Date variable equal to the systems current time. We are going to use this to set the match times up
 	
 	result.r.each { teamA ->
+	//????
     		result.r.each { teamB ->
 			if(teamA.id == teamB.id){
 			//This will make sure No games can be played where the 2 teams are the same. Which would be impossible in the real world.
@@ -44,7 +45,7 @@ class FixtureGeneratorController {
 				def fixtureName
 				//Creates a variable called fixtureName
 				if(teamA.teamName > teamB.teamName){
-					//This will order them by the alphabet. So all team games will be togther 
+					//This will order them by the alphabet in the database. So all team games will be together in the database.
 					fixtureName = "${teamA.teamName} vs ${teamB.teamName}"
 				}
 				else{
@@ -52,18 +53,24 @@ class FixtureGeneratorController {
 				}
 
 		
-
+				//if the fixture name has not been declared which at this point they shouldnt have
 				if(result.fixtures [fixtureName] == null){
-					//result.fixtures [fixtureName] = [:]
+					result.fixtures [fixtureName] = [:]
 					def newFixture = new Fixture(teamA:teamA,
 					teamB:teamB,
+ 					//This will enter a new record into the database with the fixture date pitch number and team names.
 					fixtureDate: new Date(fixture_Date), 
 					pitchNo:"${count++}", 
+					//add one to the pitch count
 					league:result.league ).save()
+					// save into the database
 					result.fixtures [fixtureName] = [fixture:newFixture,name:fixtureName]
 					if (count > 4){
 						count = 1
-						fixture_Date += (7*24*60*60*1000) 
+						fixture_Date += (7*24*60*60*1000)
+						//In our league their are only going to be 4 pitches to play on. So this will count to 4 and after 
+						//that the games will have to be arranged for another date. It will assign the games for a week in the 
+						//future. 
 					}
 				}
 			}
@@ -71,6 +78,7 @@ class FixtureGeneratorController {
 		}
 	}
 	redirect (uri:"/")
+	//redirect to home page
 	return result
 	
 
